1575b229fe7873fc91ee0fe60a46c487458ec1fb
a.schurenkov <a.schurenkov@dostaevsky.ru>
2019-07-05 12:58:30 +0300

    fix webpack

--- conf/webpack.production.config.js
+++ conf/webpack.production.config.js
@@ -2,7 +2,6 @@ import Config from 'webpack-config';
 import webpack from 'webpack';
 import autoprefixer from 'autoprefixer';
 import TerserPlugin from 'terser-webpack-plugin';
-import MomentLocalesPlugin from 'moment-locales-webpack-plugin';
 
 export default new Config().extend('conf/webpack.base.config.js').merge({
   mode: 'production',
@@ -12,8 +11,5 @@ export default new Config().extend('conf/webpack.base.config.js').merge({
   devtool: 'source-map',
   plugins: [
     new webpack.LoaderOptionsPlugin({ options: { postcss: [autoprefixer] } }),
-    new MomentLocalesPlugin({
-      localesToKeep: ['ru'],
-    })
   ]
 });
