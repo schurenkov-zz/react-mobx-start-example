2000f79a1b4ea17992ce5a3e0b753b95adfad52b
a.schurenkov <a.schurenkov@dostaevsky.ru>
2019-07-05 14:56:39 +0300

    add config for heroku

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var express = require('express');
 var app = express();
 const path = require('path');
 
-app.use(express.static('build'));
+app.use(express.static('dist'));
 app.listen(process.env.PORT || 8080);
